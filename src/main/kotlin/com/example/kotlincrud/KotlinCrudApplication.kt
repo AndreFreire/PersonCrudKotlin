6657b5f5f47faba6f725c18b5e49ff06a0d76411
AndreFreire <and__re@hotmail.com>
2018-06-13 16:16:45 -0300

    remove unused annotation

--- src/main/kotlin/com/example/kotlincrud/KotlinCrudApplication.kt
+++ src/main/kotlin/com/example/kotlincrud/KotlinCrudApplication.kt
@@ -1,13 +1,9 @@
 package com.example.kotlincrud
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@Configuration
 class KotlinCrudApplication
 
 fun main(args: Array<String>) {
